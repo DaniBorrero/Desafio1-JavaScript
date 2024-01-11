a25dc441c9e6795f59fdbbef1aea132d6860b364
DanielBorrero <borrerodaniel89@gmail.com>
2024-01-11 22:47:14 +0100

    finish

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ precio = 400000
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+count = document.querySelector(".cantidad");
+total =  document.querySelector(".valor-total");
+
+function sumar() {
+    // console.log(cantidad.innerHTML);
+    count.innerHTML = Number(count.innerHTML) +1;
+    total.innerHTML = Number(count.innerHTML) * precio;
+}
+
+function restar() {
+    if (Number(count.innerHTML) > 0) {
+        count.innerHTML = Number(count.innerHTML) -1;
+        total.innerHTML = Number(count.innerHTML) * precio;
+    }
+}
+
+
+
